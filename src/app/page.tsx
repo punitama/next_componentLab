276fa960f4ffcc6405ab8762045dee12075bc48a
Oishi <rq14jy64@gmail.com>
2024-02-14 01:13:22 +0900

    ボタン、カレンダー導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div>テスト</div>
+      <Button>テスト</Button>
+      <Calendar />
     </main>
   );
 }
